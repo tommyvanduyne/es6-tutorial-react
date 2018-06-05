086f6c03dcc01467bb6236b483ae9f8e89e88c73
Tommy Van Duyne <tommyvanduyne@gmail.com>
2018-06-05 17:03:57 +0000

    got a working version of webpack going

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var parentDir = path.join(__dirname, '../');
+
+module.exports = {
+	entry: [
+		path.join(parentDir, 'index.js')
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}, 
+			{
+				test: /\.less$/,
+				loaders: ['style-loader','css-loader','less-loader']
+			}
+		]
+	},	
+	output: {
+		path: parentDir + '/dist',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: parentDir,
+		historyApiFallback: true,
+		host:"0.0.0.0",
+		port:8080,
+		disableHostCheck:true
+	}
+}
